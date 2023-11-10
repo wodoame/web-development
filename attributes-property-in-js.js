3c2b99484ea575e206c5d7688450872ac731e1b9
Bernard Mawulorm Kofi Wodoame <98283377+wodoame@users.noreply.github.com>
2023-11-10 17:26:19 +0000

    Update attributes-property-in-js.js

--- attributes-property-in-js.js
+++ attributes-property-in-js.js
@@ -42,3 +42,37 @@
 
 // These methods allow you to interact with and manipulate the attributes of an HTML element dynamically.
 // Keep in mind that modifying attributes directly may affect the appearance or behavior of the associated element on the page.
+
+// You can use JavaScript to access and manipulate HTML element attributes. To get a list of attributes for a specific HTML element, you can use the `attributes` property.
+// Here's a brief example:
+
+
+// Assuming you have an HTML element with the id "exampleElement"
+var element = document.getElementById("exampleElement");
+
+// Get a list of attributes
+var attributeList = element.attributes;
+
+// Loop through the attributes and log them
+for (var i = 0; i < attributeList.length; i++) {
+    console.log(attributeList[i].name + ": " + attributeList[i].value);
+}
+
+// This code retrieves the HTML element with the specified ID, gets its attributes using `attributes`, and then iterates through the list to log each attribute's name and value.
+
+// When looping through the attributes using the `attributes` property, you can access the name and value of each attribute using the `name` and `value` properties of each attribute node.
+// Here's an example:
+// Just a more detailed explanation of the previous example
+
+var element = document.getElementById("exampleElement");
+var attributeList = element.attributes;
+
+for (var i = 0; i < attributeList.length; i++) {
+    var attributeName = attributeList[i].name;
+    var attributeValue = attributeList[i].value;
+
+    console.log(attributeName + ": " + attributeValue);
+}
+
+// In this loop, `attributeList[i].name` gives you the name of the current attribute, and `attributeList[i].value` gives you its value.
+// The example uses `console.log` to output the attribute name and value for each attribute in the loop. Adjust this code according to your specific needs when working with attribute values in JavaScript.
